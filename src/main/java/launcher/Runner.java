8ff16db24acbe994376d3bbc9b3b21a549b27cdc
sofianekabylino <yousfi_sofiane@live.com>
2015-12-13 13:25:28 +0100

    OK for ConstantReplacement metamutator

--- src/main/java/launcher/Runner.java
+++ src/main/java/launcher/Runner.java
@@ -1,14 +1,39 @@
 package launcher;
 
-import org.jsoup.helper.HttpConnectionTest;
-import org.jsoup.helper.StringUtilTest;
+import java.io.File;
 
 import metamutator.MutantSearchSpaceExplorator;
 
 public class Runner {
+	
+	public static void walk(String path) throws ClassNotFoundException, Exception {
+        File root = new File( path );
+        File[] list = root.listFiles();
 
+        if (list == null) return;
+
+        for ( File f : list ) {
+            if ( f.isDirectory() ) {
+                walk( f.getAbsolutePath() );
+            }
+            else {
+            	if(f.getName().endsWith(".class") && !f.getName().contains("$")){
+    				//System.out.println("********** " + tab[i].getName() + " ***********");
+    				String[] res = f.getName().split("\\.");
+    				System.out.println(res[0]);
+    				String the_path = f.getAbsolutePath().replace(".class","");
+    				String package_name = the_path.split("org/")[1].replace('/', '.');
+    				package_name.replace(".class", "");
+    				MutantSearchSpaceExplorator.runMetaProgramWith(Class.forName("org." + package_name));
+    				System.out.println();
+    			}
+            }
+        }
+    }
+    
 	public static void main(String[] args) throws Exception {
-	    //MutantSearchSpaceExplorator.runMetaProgramWith(StringUtilTest.class);
-	    MutantSearchSpaceExplorator.runMetaProgramWith(HttpConnectionTest.class);
+		
+		walk("target/classes/org/jsoup");
+
 	}
 }
