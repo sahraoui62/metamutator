f36b3dda12baa5d86923433bf76a8baad4bdb6b4
sahraoui62 <elouadi.slimane@yahoo.fr>
2015-12-16 15:02:22 +0100

    first commit

--- src/main/java/metamutator/BinaryOperatorMetaMutator.java
+++ src/main/java/metamutator/BinaryOperatorMetaMutator.java
@@ -142,8 +142,6 @@ public class BinaryOperatorMetaMutator extends
 		expression.replace(codeSnippet);
 		expression.replace(expression);
 		Selector.generateSelector(expression, originalKind, thisIndex, operators, PREFIX);
-		System.out.println("nb mutants " + thisIndex);
-
 		hostSpots.add(expression);
 
 	}
